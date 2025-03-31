16e5ecbc2ab4268c61ed13622f89f0698d6aff67
binhfdv <dvanbinh10@gmail.com>
2025-03-31 15:56:11 +0200

    update script to remove cni folder

--- install_k8s.sh
+++ install_k8s.sh
@@ -23,6 +23,8 @@ else
     fi
 fi
 
+sudo rm -rf /etc/cni /etc/kubernetes /var/lib/dockershim /var/lib/etcd /var/lib/kubelet /var/run/kubernetes ~/.kube/*
+
 # containerd installaiton and configuration
 sudo apt update
 sudo apt install -y containerd
